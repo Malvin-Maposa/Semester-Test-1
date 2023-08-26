6f65ea588d11dd20f443f02e558825b9c161e123
Malvin-Maposa <74313094+Malvin-Maposa@users.noreply.github.com>
2023-08-26 08:35:44 +0200

    Update testscript.py

--- testscript.py
+++ testscript.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn.linear_model import Lasso
 from sklearn.model_selection import train_test_split

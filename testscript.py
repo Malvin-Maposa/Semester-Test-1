b2fb00bd8097807ffc8e62e3fa3cd66de1ce3051
Malvin <malvinmaposa@gmail.com>
2023-08-26 08:50:23 +0200

    RF commit

--- testscript.py
+++ testscript.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn import metrics
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
